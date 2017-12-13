c167a9a22f5792eeb2f2b25d32f1dc2f9dc742da
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 15:04:13 -0800

    Add tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,6 @@ describe User, vcr: true do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should validate_presence_of :password_confirmation }
+  it { should validate_uniqueness_of :name }
+  it { should validate_uniqueness_of :email }
 end
