6d1a2abad578c80080c9ff695e1edf1a656462ea
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 11:31:57 -0800

    Update specs to work with API.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-describe User do
+describe User, vcr: true do
   it { should validate_presence_of :name }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should validate_presence_of :password_confirmation }
-  it { should validate_uniqueness_of :name }
-  it { should validate_uniqueness_of :email }
 end
