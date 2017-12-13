02729859626764360917b55115bf6ba7d2798fa7
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 08:35:44 -0800

    Add controllers and user model.

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'site#welcome'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+  resources :groups do
+    resources :messages
+  end
+end
