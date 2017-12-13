fc8833f20db9e985d98fd157a356b874cead12fb
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 08:45:13 -0800

    Add user tests and controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password, :password_confirmation
   validates_uniqueness_of :name, :email
 end
