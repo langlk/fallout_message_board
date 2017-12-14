917ca3b09d1ea91f9fc9105c6e201a4e0299e0e9
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-13 16:07:11 -0800

    add test for signin errors.

--- spec/features/sign_in_path_spec.rb
+++ spec/features/sign_in_path_spec.rb
@@ -1,13 +1,19 @@
 require "rails_helper"
 
 describe "user signin path", vcr: true do
-  let!(:user) { FactoryBot.create(:user, name: "Veronica", email: "v@brotherhoodofsteel.org", password: "powergauntlet", password_confirmation: "powergauntlet")}
 
   it "allows user to sign in with email and password" do
+    FactoryBot.create(:user, name: "Veronica", email: "v@brotherhoodofsteel.org", password: "powergauntlet", password_confirmation: "powergauntlet")
     visit '/signin'
     fill_in 'email', with: 'v@brotherhoodofsteel.org'
     fill_in 'password', with: 'powergauntlet'
     click_button 'Sign In'
     expect(page).to have_content('Welcome back, Veronica')
+  end
+
+  it "displays an error if user cannot be signed in" do
+    visit '/signin'
+    click_button 'Sign In'
+    expect(page).to have_content('Incorrect email or password')
   end
 end
